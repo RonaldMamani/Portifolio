96a48443e3f186fdcc9c2d83e81be18594bbf739
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-09-30 14:05:52 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ronaldo Mamani | Portifolio',
+  description: 'Meu portifolio pessoal',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
