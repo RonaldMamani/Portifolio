084c7441dce3ed5c6642ba71e0acb183b41acf3d
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-10-05 11:47:30 -0300

    Adicionando componentes personalizado do Slide do Swipper, e personalizando a barra de scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Habilidades from "@/components/Habilidades";
 import Header from "@/components/Header";
 import HomePage from "@/components/HomePage";
-import Projetos from "@/components/Projetos";
-import SwiperJs from "@/components/Projetos/Swiper";
 import Sobre from "@/components/Sobre";
+import Habilidades from "@/components/Habilidades";
+import Projetos from "@/components/Projetos";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 
 
 export default function Home() {
--- src/components/Projetos/slide.tsx
+++ src/components/Projetos/slide.tsx
@@ -1,62 +1,65 @@
-"use client"
-
 import React, { useState } from "react";
-import { SwiperSlide } from "swiper/react";
-import Image from "next/image";
+import {SwiperSlide } from "swiper/react";
+import Image, { StaticImageData } from "next/image";
 import TextSecundary from "../Texts/TextSecundary";
 import TextCommon from "../Texts/TextCommon";
 import ButtonLink from "../Buttons/ButtonLink";
+import { SiHtml5, SiCss3, SiJavascript, SiReact, SiTailwindcss, SiBootstrap, SiStyledcomponents, SiSass, SiNextdotjs, SiTypescript, SiNodedotjs } from "@icons-pack/react-simple-icons";
 
-import { SiReact, SiHtml5, SiCss3, SiJavascript, SiTailwindcss, SiBootstrap, SiStyledcomponents, SiNodedotjs, SiTypescript, SiSass, SiNextdotjs } from "@icons-pack/react-simple-icons"
-
-interface SlideProps {
-    imageSrc: string;
+type SlideProps = {
+    imageSrc: StaticImageData;
     title: string;
     description: string;
     gitLink: string;
     hostedLink: string;
     technologies: string[];
-}
+};
 
-const Slide: React.FC<SlideProps> = ({ imageSrc, title, description, gitLink, hostedLink, technologies }) => {
+export default function Slide({ imageSrc, title, description, gitLink, hostedLink, technologies } : SlideProps) {
     const [onHover, setOnHover] = useState(false);
 
     return (
         <SwiperSlide
-            onMouseEnter={() => setOnHover(!onHover)}
-            onMouseLeave={() => setOnHover(!onHover)}
-            className="relative"
-        >
+            onMouseEnter={() => setOnHover(true)}
+            onMouseLeave={() => setOnHover(false)}
+            >
             <Image className="rounded-3xl" src={imageSrc} alt="slide_image" />
             <div
-                className={`${onHover ? "absolute bottom-0 bg-black/95 h-full xl:h-[70%] w-full" : "hidden"} 
-                            rounded-xl transition-transform py-3 px-4 
-                            md:py-1 md:px-3 
-                            lg:p-4`}
-            >
-                <div className="py-4 flex flex-col gap-8 items-center">
-                    <TextSecundary className="text-3xl font-extrabold" text={title} />
+                className={`${
+                onHover ? "absolute bottom-0 bg-black/95 h-full xl:h-[75%] w-full" : "hidden"
+                } 
+                rounded-xl transition-transform py-3 px-4 
+                md:py-1 md:px-3 
+                lg:p-4`}>
+                <div className="py-4 flex flex-col gap-6 lg:gap-8 items-center">
+                    <TextSecundary className="text-3xl font-extrabold uppercase" text={title} />
                     <div className="flex flex-col gap-3">
-                        <TextCommon className="text-gray-300 font-bold" text="Descrição: " />
+                        <TextCommon className="text-gray-300 font-bold uppercase" text="Descrição: " />
                         <TextCommon className="text-sm text-gray-300" text={description} />
                     </div>
                     <div className="flex flex-col items-center gap-3">
-                        <TextCommon className="text-gray-300 font-bold" text="Links:" />
+                        <TextCommon className="text-gray-300 font-bold uppercase" text="Links:" />
                         <div className="flex flex-col md:flex-row gap-5">
                             <ButtonLink link={gitLink} content="Repositório GIT" />
                             <ButtonLink link={hostedLink} content="Site Hospedado" />
                         </div>
                     </div>
                     <div className="flex flex-col gap-3">
-                        <TextCommon className="text-gray-300 font-bold" text="Tecnologias Usadas:" />
-                        <div className="flex gap-5">
+                        <TextCommon className="text-gray-300 font-bold uppercase" text="Tecnologias Usadas:" />
+                        <div className="flex gap-5 justify-center items-center">
                             {technologies.map((tech, index) => (
                                 <span key={index} title={tech}>
-                                    {tech === "HTML" && <SiHtml5 width={30} height={30} color="#E34F26" />}
-                                    {tech === "CSS" && <SiCss3 width={30} height={30} color="#1572B6" />}
-                                    {tech === "JavaScript" && <SiJavascript width={30} height={30} color="#F7DF1E" />}
-                                    {tech === "ReactJs" && <SiReact width={30} height={30} color="#61DAFB" />}
-                                    {tech === "TailwindCSS" && <SiTailwindcss width={30} height={30} color="#06B6D4" />}
+                                    {tech === "HTML" && <SiHtml5 width={30} height={30} color="#E34F26" title="HTML" />}
+                                    {tech === "CSS" && <SiCss3 width={30} height={30} color="#1572B6" title="CSS" />}
+                                    {tech === "JavaScript" && <SiJavascript width={30} height={30} color="#F7DF1E" title="JavaScript" />}
+                                    {tech === "ReactJs" && <SiReact width={30} height={30} color="#61DAFB" title="ReactJs" />}
+                                    {tech === "TailwindCSS" && <SiTailwindcss width={30} height={30} color="#06B6D4" title="TailwindCSS" />}
+                                    {tech === "Bootstrap" && <SiBootstrap width={30} height={30} color="#7952B3" title="Bootstrap5" />}
+                                    {tech === "StyledComponents" && <SiStyledcomponents width={30} height={30} color="#D87893" title="StyledComponents" />}
+                                    {tech === "Sass" && <SiSass width={30} height={30} color="#CC6699" title="Sass" />}
+                                    {tech === "NextJs" && <SiNextdotjs width={30} height={30} color="#000" title="NextJs" />}
+                                    {tech === "TypeScript" && <SiTypescript width={30} height={30} color="#3178C6" title="TypeScript" />}
+                                    {tech === "NodeJs" && <SiNodedotjs width={30} height={30} color="#339933" title="NodeJs" />}
                                 </span>
                             ))}
                         </div>
@@ -66,5 +69,3 @@ const Slide: React.FC<SlideProps> = ({ imageSrc, title, description, gitLink, ho
         </SwiperSlide>
     );
 };
-
-export default Slide;
